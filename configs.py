3c8e3254815af2002f12f47b26f3c9215af8d71a
Nicolas Audinet de Pieuchon <nic.audinet27@gmail.com>
2024-02-02 00:39:21 +0100

    Basic pygame works
    Still no wall detection or proper graphics

--- configs.py
+++ configs.py
@@ -1,8 +1,12 @@
-from typing import Tuple
+from typing import List
+
 from pydantic import BaseModel
+
 
 class BaseConfig(BaseModel):
     speed: float = 1
-    angular_speed: float = .2
-    board_size: Tuple[int] = [100, 100]
+    angular_speed: float = 0.2
+    board_size: List[int] = [100, 100]
     n_players: int = 2
+    scale: int = 10
+    fps: int = 5
--- game.py
+++ game.py
@@ -1,20 +1,28 @@
-from typing import Tuple
 from enum import Enum
+from itertools import product
+from typing import List
 
-import matplotlib.pyplot as plt
 import gymnasium as gym
-from gymnasium import spaces
 import numpy as np
+import pygame as pg
+from gymnasium import spaces
 
 from configs import BaseConfig
+
 
 class Action(Enum):
     LEFT = 0
     RIGHT = 1
 
+
 class Player:
     def __init__(self, id, x, y, speed, angular_speed):
         self.id = id
+        self.color = pg.Color(
+            np.random.randint(low=0, high=255),
+            np.random.randint(low=0, high=255),
+            np.random.randint(low=0, high=255),
+        )
         self.x = x
         self.y = y
         self.direction = np.random.rand() * 2 * np.pi
@@ -25,10 +33,8 @@ class Player:
     def move(self, action):
         if not self.alive:
             return
-        
-        print(self.direction)
-        print(self.x)
-        print(self.y)
+
+        print(f"Player {self.id}: ({self.x}, {self.y}) -> {self.direction}")
 
         if action == Action.LEFT:
             self.direction -= self.angular_speed
@@ -42,63 +48,117 @@ class Player:
 
 
 class KurveEnv(gym.Env):
-    def __init__(self, n_players: int, board_size: Tuple[int], speed: float, angular_speed: float):
+    def __init__(
+        self,
+        n_players: int,
+        board_size: List[int],
+        speed: float,
+        angular_speed: float,
+        scale: int,
+        fps: int,
+        render_mode: str = "human",
+    ):
         super(KurveEnv, self).__init__()
-        
-        self.players = None
+
+        self.players = []
         self.board_size = board_size
         self.n_players = n_players
         self.speed = speed
         self.angular_speed = angular_speed
+        self.render_mode = render_mode
+        self.scale = scale
+        self.fps = fps
+
+        self.window = None
+        self.clock = None
 
         self.action_space = spaces.Discrete(2)
-        self.observation_space = spaces.Box(low=0, high=self.n_players, shape=board_size, dtype=int)
+        self.observation_space = spaces.Box(
+            low=0, high=self.n_players, shape=board_size, dtype=np.int32
+        )
 
     def step(self, actions):
-        assert len(actions) == len(self.players), 'Actions missmatch'
+        assert len(actions) == len(self.players), "Actions missmatch"
 
         for p, a in zip(self.players, actions):
+            print(f"Player {p.id} is alive: {p.alive}")
             p.move(a)
 
         for p in self.players:
             x, y = p.to_grid()
-            if self.board[y, x] != 0:
+            if self.board[y, x] not in (0, p.id):
                 p.alive = False
             else:
                 self.board[y, x] = p.id
 
         reward = 0
-        done = not any([p.alive for p in self.players])
-        return self.board, reward, done, {}
+        terminated = not any([p.alive for p in self.players])
+        return self.board, reward, terminated, {}
 
     def reset(self):
         self.players = []
         initial_pos = np.random.randint(0, self.board_size[0], size=(self.n_players, 2))
+        colors = [pg.Color("red"), pg.Color("green")]
         for i in range(self.n_players):
             x, y = initial_pos[i]
-            p = Player(i, x, y, self.speed, self.angular_speed)
+            p = Player(i + 1, x, y, self.speed, self.angular_speed)
+            p.color = colors[i]
             self.players.append(p)
 
-        self.board = np.zeros(self.board_size)
+        self.board = np.zeros(self.board_size, dtype=np.int32)
         for p in self.players:
-            self.board[p.y, p.x] = p.id + 1
+            self.board[p.y, p.x] = p.id
+        self.colors = [p.color for p in self.players]
 
         return self.board
 
-    def render(self, mode='human'):
-        plt.imshow(self.board)
-        plt.show()
+    def _render_frame(self):
+        pg.init()
+        pg.display.set_caption("Kurve")
+        window_size = (self.board_size[0] * self.scale, self.board_size[1] * self.scale)
+        if self.window is None:
+            self.window = pg.display.set_mode(window_size)
+        if self.clock is None:
+            self.clock = pg.time.Clock()
+
+        canvas = pg.Surface(window_size)
+        canvas.fill(pg.Color("black"))
+
+        rows = range(self.board_size[0])
+        cols = range(self.board_size[1])
+        for x, y in product(rows, cols):
+            pixel = self.board[y, x]
+            if pixel > 0:
+                print(pixel)
+                border = (x * self.scale, y * self.scale, self.scale, self.scale)
+                pg.draw.rect(canvas, self.colors[pixel - 1], border)
+
+        self.window.blit(canvas, canvas.get_rect())
+        pg.event.pump()
+        pg.display.update()
+        self.clock.tick(self.fps)
+
+    def render(self):
+        if self.render_mode == "human":
+            self._render_frame()
 
 
 def main():
     config = BaseConfig()
 
-    env = KurveEnv(n_players=config.n_players, board_size=config.board_size, speed=config.speed,
-                   angular_speed=config.angular_speed)
+    env = KurveEnv(
+        n_players=config.n_players,
+        board_size=config.board_size,
+        speed=config.speed,
+        angular_speed=config.angular_speed,
+        render_mode="human",
+        scale=config.scale,
+        fps=config.fps,
+    )
 
     observation = env.reset()
 
-    for _ in range(1000):
+    for _ in range(20):
         # action = env.action_space.sample()
         actions = [Action.LEFT, Action.LEFT]
         observation, reward, terminated, info = env.step(actions)
@@ -109,5 +169,6 @@ def main():
 
     env.close()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
